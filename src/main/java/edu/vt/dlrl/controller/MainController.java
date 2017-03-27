60e6de74deb9bd19e38256aadcb51f72e174f36f
DedoCibula <dedo.cibula@gmail.com>
2017-03-26 22:50:08 -0400

    aadded sample view and controller, removed wrong annotation

---
+++ src/main/java/edu/vt/dlrl/controller/MainController.java
@@ -0,0 +1,18 @@
+package edu.vt.dlrl.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Author: dedocibula
+ * Created on: 26.3.2017.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String home() {
+        return "index";
+    }
+}
