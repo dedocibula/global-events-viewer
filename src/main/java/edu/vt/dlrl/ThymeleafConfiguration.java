9ebb5f4d4e40f137badcee8825f966a4020a1dcb
DedoCibula <dedo.cibula@gmail.com>
2017-04-10 16:06:36 -0400

    added Thymeleaf layout dialect, split rendering logic

--- src/main/java/edu/vt/dlrl/ThymeleafConfiguration.java
+++ src/main/java/edu/vt/dlrl/ThymeleafConfiguration.java
@@ -1,9 +1,11 @@
 package edu.vt.dlrl;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.dialect.IDialect;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 import org.thymeleaf.templateresolver.TemplateResolver;
@@ -31,5 +33,10 @@ public class ThymeleafConfiguration {
         resolver.setCharacterEncoding(properties.getEncoding().name());
         resolver.setCacheable(properties.isCache());
         return resolver;
+    }
+
+    @Bean
+    public IDialect layoutDialect(){
+        return new LayoutDialect();
     }
 }
